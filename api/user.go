956f607b8375b4ea8f93f6df60b06de166aca14b
CocaineCong <294350394@qq.com>
2021-10-03 22:40:09 +0800

    feat:优化用户注册登录结构

--- api/user.go
+++ api/user.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"to-do-list/pkg/logging"
-	"to-do-list/serializer"
 	"to-do-list/service"
 )
 
@@ -13,9 +12,9 @@ func UserRegister(c *gin.Context) {
 	session := sessions.Default(c)
 	status := 200
 	userID := session.Get("userId")
-	var service service.UserRegisterService //相当于创建了一个UserRegisterService对象，调用这个对象中的Register方法。
-	if err := c.ShouldBind(&service); err == nil {
-		res := service.Register(userID, status)
+	var userRegisterService service.UserRegisterService //相当于创建了一个UserRegisterService对象，调用这个对象中的Register方法。
+	if err := c.ShouldBind(&userRegisterService); err == nil {
+		res := userRegisterService.Register(userID, status)
 		c.JSON(200, res)
 	} else {
 		c.JSON(200, ErrorResponse(err))
@@ -28,21 +27,12 @@ func UserLogin(c *gin.Context) {
 	session := sessions.Default(c)
 	status := 200
 	userID := session.Get("userId")
-	var service service.UserLoginService
-	if err := c.ShouldBind(&service); err == nil {
-		res := service.Login(userID, status)
+	var userLoginService service.UserLoginService
+	if err := c.ShouldBind(&userLoginService); err == nil {
+		res := userLoginService.Login(userID, status)
 		c.JSON(200, res)
 	} else {
 		c.JSON(200, ErrorResponse(err))
 		logging.Info(err)
 	}
-}
-
-
-//CheckToken 用户详情
-func CheckToken(c *gin.Context) {
-	c.JSON(200, serializer.Response{
-		Status: 200,
-		Msg:    "ok",
-	})
 }
--- serializer/user.go
+++ serializer/user.go
@@ -5,12 +5,7 @@ import "to-do-list/model"
 type User struct {
 	ID       uint   `json:"id"`
 	UserName string `json:"user_name"`
-	NickName string `json:"nickname"`
-	Type     int    `json:"type"`
-	Email    string `json:"email"`
 	Status   string `json:"status"`
-	Avatar   string `json:"avatar"`
-	Monery   int `json:"monery"`
 	CreateAt int64  `json:"create_at"`
 }
 
