6bd9dde05d02e4e7dcc23cc1657a8985f8242a9a
CocaineCong <294350394@qq.com>
2021-11-11 16:48:06 +0800

    feat:fix foreign key

--- model/init.go
+++ model/init.go
@@ -11,13 +11,6 @@ var DB *gorm.DB
 
 func Database(connString string) {
 	db, err := gorm.Open("mysql", connString)
-	//db, err = gorm.Open(mysql.Open(dns), &gorm.Config{
-	//	DisableForeignKeyConstraintWhenMigrating: true,// 外键约束
-	//	SkipDefaultTransaction: true, 	// 禁用默认事务（提高运行速度）
-	//	NamingStrategy: schema.NamingStrategy{
-	//		SingularTable: true, // 使用单数表名，启用该选项，此时，`User` 的表名应该是 `user`
-	//	},
-	//})
 	db.LogMode(true)
 	if err != nil {
 		panic(err)
@@ -25,11 +18,10 @@ func Database(connString string) {
 	if gin.Mode() == "release" {
 		db.LogMode(false)
 	}
-	db.SingularTable(true)       //默认不加复数s
+	db.SingularTable(true)   //默认不加复数s
 	db.DB().SetMaxIdleConns(20)  //设置连接池，空闲
 	db.DB().SetMaxOpenConns(100) //打开
 	db.DB().SetConnMaxLifetime(time.Second * 30)
-	//db.Model(&Boss{}).AddForeignKey("ProductID", "Product(ProductID)", "RESTRICT", "RESTRICT")
 	DB = db
 	migration()
 }
