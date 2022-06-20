3888bbef24c4dfb6571fadd4847dc3328d3640c7
cengqingcong <cengqingcong@shizhuang-inc.com>
2022-06-21 01:40:00 +0800

    feat gorm v2

--- model/migration.go
+++ model/migration.go
@@ -3,8 +3,10 @@ package model
 //执行数据迁移
 func migration() {
 	//自动迁移模式
-	DB.Set("gorm:table_options", "charset=utf8mb4").
-		AutoMigrate(&User{}).
-		AutoMigrate(&Task{})
-	DB.Model(&Task{}).AddForeignKey("uid","User(id)","CASCADE","CASCADE")
+	err := DB.Set("gorm:table_options", "charset=utf8mb4").
+		AutoMigrate(&User{}, &Task{})
+	if err != nil {
+		return
+	}
+	//DB.Model(&Task{}).AddForeignKey("uid","User(id)","CASCADE","CASCADE")
 }
--- model/task.go
+++ model/task.go
@@ -12,22 +12,21 @@ type Task struct {
 	User      User   `gorm:"ForeignKey:Uid"`
 	Uid       uint   `gorm:"not null"`
 	Title     string `gorm:"index;not null"`
-	Status    int    `gorm:"default:'0'"`
+	Status    int    `gorm:"default:0"`
 	Content   string `gorm:"type:longtext"`
 	StartTime int64
-	EndTime   int64 `gorm:"default:'0'"`
+	EndTime   int64 `gorm:"default:0"`
 }
 
 func (Task *Task) View() uint64 {
 	//增加点击数
- 	countStr, _ := cache.RedisClient.Get(cache.TaskViewKey(Task.ID)).Result()
+	countStr, _ := cache.RedisClient.Get(cache.TaskViewKey(Task.ID)).Result()
 	count, _ := strconv.ParseUint(countStr, 10, 64)
 	return count
 }
 
 //AddView
 func (Task *Task) AddView() {
-	cache.RedisClient.Incr(cache.TaskViewKey(Task.ID))	//增加视频点击数
-	cache.RedisClient.ZIncrBy(cache.RankKey, 1, strconv.Itoa(int(Task.ID)))	//增加排行点击数
+	cache.RedisClient.Incr(cache.TaskViewKey(Task.ID))                      //增加视频点击数
+	cache.RedisClient.ZIncrBy(cache.RankKey, 1, strconv.Itoa(int(Task.ID))) //增加排行点击数
 }
-
