1b2876cbfed48cac3e71dc9a1cb379283da19f46
FanOne <294350394@qq.com>
2023-01-17 22:36:58 +0800

    feat:format

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import (
 // @name FanOne
 // @BasePath /api/v1
 func main() { // http://localhost:3000/swagger/index.html
-	//从配置文件读入配置
+	// 从配置文件读入配置
 	conf.Init()
-	//转载路由 swag init -g common.go
+	// 转载路由 swag init -g common.go
 	r := routes.NewRouter()
 	_ = r.Run(conf.HttpPort)
 }
